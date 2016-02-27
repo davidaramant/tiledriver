6abfa1977616776397ba4f573ba3bd15b0c02e9f
David Aramant <aramant@sep.com>
2016-02-27 15:12:12 -0500

    Added RegionTheme class

---
+++ Tiledriver/Tiledriver/Generator/RegionTheme.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiledriver.Wolf3D;
+
+namespace Tiledriver.Generator
+{
+    // TODO: Change how likely it is for each type of thing to be spawned
+    public sealed class RegionTheme
+    {
+        public readonly List<TileTheme> NormalWalls;
+        public readonly List<TileTheme> DecorationWalls;
+
+        public readonly List<WolfActor> OrderedDecorations;
+        public readonly List<WolfActor> RandomDecorations;
+
+        public readonly List<WolfActor> EnemyTypes;
+
+        private RegionTheme(
+            IEnumerable<TileTheme> normalWalls,
+            IEnumerable<TileTheme> decorationWalls,
+            IEnumerable<WolfActor> orderedDecorations,
+            IEnumerable<WolfActor> randomDecorations,
+            IEnumerable<WolfActor> enemyTypes)
+        {
+            NormalWalls = normalWalls.ToList();
+            DecorationWalls = decorationWalls.ToList();
+            OrderedDecorations = orderedDecorations.ToList();
+            RandomDecorations = randomDecorations.ToList();
+            EnemyTypes = enemyTypes.ToList();
+        }
+
+        public static readonly RegionTheme Dungeon = new RegionTheme(
+            normalWalls: new[]
+            {
+                TileTheme.BlueStone1,
+                TileTheme.BlueStone2,
+            },
+            decorationWalls: new[]
+            {
+                TileTheme.BlueStoneSign,
+            },
+            orderedDecorations: new WolfActor[] { },
+            randomDecorations: new WolfActor[] { },
+            enemyTypes: new WolfActor[] { } );
+
+        public static readonly RegionTheme OfficerLounge = new RegionTheme(
+            normalWalls: new[]
+            {
+                TileTheme.Wood,
+            },
+            decorationWalls: new[]
+            {
+                TileTheme.WoodCross,
+                TileTheme.WoodEagle,
+                TileTheme.WoodHitler,
+            },
+            orderedDecorations: new WolfActor[] { },
+            randomDecorations: new WolfActor[] { },
+            enemyTypes: new WolfActor[] { });
+
+        public static readonly RegionTheme StoneHallway = new RegionTheme(
+            normalWalls: new[]
+            {
+                TileTheme.GrayStone1,
+                TileTheme.GrayStone2,
+                TileTheme.GrayStone3,
+            },
+            decorationWalls: new[]
+            {
+                TileTheme.GrayStoneEagle,
+                TileTheme.GrayStoneHitler,
+                TileTheme.GrayStoneFlag,
+            },
+            orderedDecorations: new WolfActor[] { },
+            randomDecorations: new WolfActor[] { },
+            enemyTypes: new WolfActor[] { });
+    }
+}
