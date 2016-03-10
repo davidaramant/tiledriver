303172a45d8a88fc356e49ea6f6b056442f1f0dd
David Aramant <davidaramant@gmail.com>
2016-03-09 21:03:11 -0500

    Renaming the convenience launcher to be more clear

--- Tiledriver/ECWolfLauncher/Program.cs
+++ Tiledriver/ECWolfLauncher/Program.cs
@@ -7,7 +7,7 @@ using Tiledriver;
 namespace TestRunner
 {
     /// <summary>
-    /// Convenience program to directly launch the output in ECWolf from Visual Studio.
+    /// Convenience program to directly launch the output in ECWolf from inside Visual Studio.
     /// </summary>
     class Program
     {
@@ -21,18 +21,23 @@ namespace TestRunner
                     $"Could not find {inputFile}.  " +
                     "Create this file in the output directory containing a single line with the full path to ECWolf.exe.  " +
                     "Do not quote the path.");
-            } 
+            }
 
             var ecWolfPath = File.ReadAllLines(inputFile).Single();
 
-            var wadFileName = Path.GetFullPath("demo.wad" );
+            if (Path.GetFileName(ecWolfPath).ToLowerInvariant() != "ecwolf.exe")
+            {
+                ecWolfPath = Path.Combine(ecWolfPath, "ecwolf.exe");
+            }
+
+            var wadFileName = Path.GetFullPath("demo.wad");
 
             WadFile.Save(
                 DemoMap.Create(),
                 wadFileName);
 
             Process.Start(
-                ecWolfPath, 
+                ecWolfPath,
                 $"--file \"{wadFileName}\" --normal --nowait --tedlevel map01");
         }
     }
