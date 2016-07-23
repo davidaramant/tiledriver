2c91fd2f3a5df6dd4f304f454dfeb20f2491b2b8
Ryan Clarke <ryan@ryanclarke.net>
2016-07-23 16:03:07 -0400

    Added open file and draw basic walls and doors

---
+++ Tiledriver/Tiledriver.UwmfViewer/ViewModels/BaseNotifyingVm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiledriver.UwmfViewer.ViewModels
+{
+    public class BaseNotifyingVm : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void NotifySet<T>(ref T existing, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (existing?.Equals(value) ?? value != null)
+            {
+                existing = value;
+                NotifyPropertyChanged(propertyName);
+            }
+        }
+    }
+}
---
+++ Tiledriver/Tiledriver.UwmfViewer/Views/Square.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Tiledriver.Core.Uwmf;
+using Tiledriver.UwmfViewer.Utilities;
+
+namespace Tiledriver.UwmfViewer.Views
+{
+    public class Square
+    {
+        private const int dimension = 8;
+
+        private int canvasX;
+        private int canvasY;
+
+        public Tile Tile { get; set; }
+
+        public Square(Map map, int x, int y)
+        {
+            canvasX = x * dimension;
+            canvasY = y * dimension;
+
+            var tileSpace = map.TileSpaceAt(x, y);
+
+            Tile = map.TileAt(tileSpace.Tile);
+        }
+
+        public UIElement ToUIElement()
+        {
+            var rect = new Rectangle()
+            {
+                Height = dimension,
+                Width = dimension,
+                Fill = FillColor(),
+                Stroke = FillColor(),
+                StrokeThickness = 0,
+                Margin = new Thickness(0)
+            };
+
+            Canvas.SetLeft(rect, canvasX);
+            Canvas.SetTop(rect, canvasY);
+
+            return rect;
+        }
+
+        private SolidColorBrush FillColor()
+        {
+            if (Tile == null)
+            {
+                return Colors.Black.ToBrush();
+            }
+
+            if (Tile.TextureNorth.StartsWith("DOOR"))
+            {
+                return Colors.DarkBlue.ToBrush();
+            }
+            if (Tile.TextureNorth.StartsWith("SLOT"))
+            {
+                return Colors.DarkBlue.ToBrush();
+            }
+            return Colors.LightGray.ToBrush();
+        }
+    }
+}
