8f97326127e1323180797269634a52deff4c6d7b
David Aramant <davidaramant@gmail.com>
2016-07-23 13:42:40 -0400

    Adding starting room to Demo Map

--- Tiledriver/Tiledriver.Core/DemoMap.cs
+++ Tiledriver/Tiledriver.Core/DemoMap.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2016 David Aramant
 // Distributed under the GNU GPL v2. For full terms see the file LICENSE.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Tiledriver.Core.Uwmf;
@@ -29,6 +30,19 @@ namespace Tiledriver.Core
                         textureEast: "GSTONEA2",
                         textureWest: "GSTONEA2"
                     ),
+                    new Tile
+                    (
+                        textureNorth: "SLOT1_2",
+                        textureSouth: "SLOT1_2",
+                        textureWest: "DOOR1_2",
+                        textureEast: "DOOR1_2",
+                        offsetVertical: true,
+                        offsetHorizontal: false,
+                        blockingNorth: true,
+                        blockingSouth: true,
+                        blockingWest: true,
+                        blockingEast: true
+                    ),
                 },
                 sectors: new[]
                 {
@@ -36,7 +50,19 @@ namespace Tiledriver.Core
                     (
                         textureCeiling: "#C0C0C0",
                         textureFloor: "#A0A0A0"
-                    )
+                    ),
+                    new Sector
+                    (
+                        textureCeiling: "#00FF00",
+                        textureFloor: "#00FF00",
+                        comment:"Good thing"
+                    ),
+                    new Sector
+                    (
+                        textureCeiling: "#FF0000",
+                        textureFloor: "#FF0000",
+                        comment:"Invalid thing"
+                    ),
                 },
                 zones: new[]
                 {
@@ -58,69 +84,100 @@ namespace Tiledriver.Core
                         skill3: true,
                         skill4: true
                     ),
-                }.Concat(GenerateThings()),
-                triggers: new Trigger[] { }
+                },
+                triggers: new[]
+                {
+                    new Trigger
+                    (
+                        x: 2,
+                        y: 1,
+                        z: 0,
+                        activateNorth: true,
+                        activateSouth: true,
+                        activateWest: true,
+                        activateEast: true,
+                        action: "Door_Open",
+                        arg0: 1,
+                        arg1: 16,
+                        arg2: 300,
+                        arg3: 0,
+                        arg4: 0,
+                        playerUse: true,
+                        monsterUse: true,
+                        playerCross: false,
+                        repeatable: true,
+                        secret: false
+                    ),
+                }
             );
+
+            // Make the starting nook
+            map.PlaneMaps[0].TileSpaces[1 * 64 + 2].Tile = 1;
+            map.PlaneMaps[0].TileSpaces[1 * 64 + 2].Tag = 1;
+
+            map.PlaneMaps[0].TileSpaces[2 * 64 + 1].Tile = 0;
+            map.PlaneMaps[0].TileSpaces[2 * 64 + 2].Tile = 0;
+
+
+            GenerateThings(map);
 
             return map;
         }
 
-        private static IEnumerable<Thing> GenerateThings()
+        private static void GenerateThings(Map map)
         {
-            return Actor.GetAll().Where(a => a.Category == "Decorations").SelectMany(((actor, i) => new[]
+            // Decorations
+            map.Things.AddRange(Actor.GetAll().Where(a => a.Category == "Decorations").Select((actor, i) =>
             {
-                new Thing(
+                var x = 4 + 4 * (int)(i / 60);
+                var y = 2 + (i % 60);
+
+                map.PlaneMaps.First().TileSpaces[y * 64 + x].Sector = actor.Wolf3D ? 1 : 2;
+
+                return new Thing(
                     type: actor.ClassName,
-                    x: 2.5 + 4*(int)(i / 60),
-                    y: 2.5 + (i % 60),
+                    x: x + 0.5,
+                    y: y + 0.5,
                     z: 0,
                     angle: 0,
                     skill1: true,
                     skill2: true,
                     skill3: true,
-                    skill4: true),
-                new Thing(
-                    type: actor.Wolf3D? Actor.Barrel.ClassName : Actor.HangedMan.ClassName,
-                    x: 3.5 + 4*(int)(i / 60),
-                    y: 2.5 + (i % 60),
-                    z: 0,
-                    angle: 0,
-                    skill1: true,
-                    skill2: true,
-                    skill3: true,
-                    skill4: true),
+                    skill4: true);
             }));
+
+
         }
 
         private static IEnumerable<TileSpace> CreateGeometry(int width, int height)
         {
             var entries = new TileSpace[height, width];
 
-            var solidTile = new TileSpace(0, 0, -1);
-            var emptyTile = new TileSpace(-1, 0, 0);
+            Func<TileSpace> solidTile = () => new TileSpace(0, 0, -1);
+            Func<TileSpace> emptyTile = () => new TileSpace(-1, 0, 0);
 
             // ### Build a big empty square
 
             // Top wall
             for (var col = 0; col < width; col++)
             {
-                entries[0, col] = solidTile;
+                entries[0, col] = solidTile();
             }
 
             for (var row = 1; row < height - 1; row++)
             {
-                entries[row, 0] = solidTile;
+                entries[row, 0] = solidTile();
                 for (var col = 1; col < width - 1; col++)
                 {
-                    entries[row, col] = emptyTile;
+                    entries[row, col] = emptyTile();
                 }
-                entries[row, width - 1] = solidTile;
+                entries[row, width - 1] = solidTile();
             }
 
             // bottom wall
             for (var col = 0; col < width; col++)
             {
-                entries[height - 1, col] = solidTile;
+                entries[height - 1, col] = solidTile();
             }
 
 
