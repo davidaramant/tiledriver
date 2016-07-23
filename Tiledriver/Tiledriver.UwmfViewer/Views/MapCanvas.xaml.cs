195e0d86304060ffb1fb19ce91222129258bf56f
Ryan Clarke <ryan@ryanclarke.net>
2016-07-23 18:36:56 -0400

    Added panning and things

--- Tiledriver/Tiledriver.UwmfViewer/Views/MapCanvas.xaml.cs
+++ Tiledriver/Tiledriver.UwmfViewer/Views/MapCanvas.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -12,6 +13,9 @@ namespace Tiledriver.UwmfViewer.Views
     /// </summary>
     public partial class MapCanvas : UserControl
     {
+        private int squareSize = 32;
+        private SquareFactory squareFactory;
+
         public MapCanvas()
         {
             InitializeComponent();
@@ -19,18 +23,46 @@ namespace Tiledriver.UwmfViewer.Views
 
         public void Update(Map map)
         {
+            FullArea.Height = map.Height * squareSize;
+            FullArea.Width = map.Width * squareSize;
+            squareFactory = new SquareFactory(map, squareSize);
+
             for (int x = 0; x < map.Width; x++)
             {
                 for (int y = 0; y < map.Height; y++)
                 {
-                    Add(new Square(map, x, y).ToUIElement());
+                    Add(squareFactory.ForCoordinates(x, y).ToUIElements());
                 }
             }
+
+            map.Things.ForEach(t =>
+            {
+                Polygon element = Thing("8,0 16,16 0,16", Colors.Yellow);
+                Canvas.SetLeft(element, t.X * squareSize - element.Width / 2);
+                Canvas.SetTop(element, t.Y * squareSize - element.Height / 2);
+                Add(element);
+            });
+        }
+
+        private Polygon Thing(string shape, Color color)
+        {
+            return new Polygon()
+            {
+                Height = squareSize / 2,
+                Width = squareSize / 2,
+                Points = shape.ToPointsCollection(),
+                Fill = color.ToBrush()
+            };
         }
 
         public void Add(UIElement element)
         {
             FullArea.Children.Add(element);
+        }
+
+        public void Add(List<UIElement> elements)
+        {
+            elements.ForEach(Add);
         }
     }
 }
