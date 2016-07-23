b1bdcaabccb02115d7300d4df49dbc37afb11550
Ryan Clarke <ryan@ryanclarke.net>
2016-07-23 16:03:07 -0400

    Added open file and draw basic walls and doors

--- Tiledriver/Tiledriver.UwmfViewer/MainWindow.xaml.cs
+++ Tiledriver/Tiledriver.UwmfViewer/MainWindow.xaml.cs
@@ -1,22 +1,47 @@
-﻿namespace Tiledriver.UwmfViewer
+﻿using System.IO;
+using Tiledriver.Core.Uwmf.Parsing;
+using Tiledriver.UwmfViewer.ViewModels;
+
+namespace Tiledriver.UwmfViewer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private MainVm vm = new MainVm();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = vm;
+            vm.PropertyChanged += SubscribeMapCanvasToMapChanges;
+        }
+
+        public void SubscribeMapCanvasToMapChanges(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals(nameof(vm.Map)))
+            {
+                MapCanvas.Update(vm.Map);
+            }
         }
 
         private void OpenMapFile(object sender, System.Windows.RoutedEventArgs e)
         {
-            var dialog = new Microsoft.Win32.OpenFileDialog() { Multiselect = false };
+            var dialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Multiselect = false,
+                FileName = "TEXTMAP.txt"
+            };
             var result = dialog.ShowDialog();
-            if (result == true)
+            if (result == true && dialog.CheckFileExists)
             {
                 App.Current.MainWindow.Title = $"Tiledriver UWMF Viewer - {dialog.SafeFileName}";
+                using (var stream = dialog.OpenFile())
+                {
+                    vm.Map = Parser.Parse(new Lexer(new UwmfCharReader(stream)));
+                }
             }
         }
 
---
+++ Tiledriver/Tiledriver.UwmfViewer/Views/ViewExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace Tiledriver.UwmfViewer.Views
+{
+    static class ViewExtensions
+    {
+        public static SolidColorBrush ToBrush(this Color color) => new SolidColorBrush(color);
+    }
+}
