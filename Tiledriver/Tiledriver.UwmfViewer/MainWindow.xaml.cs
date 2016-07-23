0087c562a080e1702910db9e2aec39539e4e4d08
Ryan Clarke <ryan@ryanclarke.net>
2016-07-23 11:06:15 -0400

    Create basic WPF skeleton for Viewer

---
+++ Tiledriver/Tiledriver.UwmfViewer/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Tiledriver.UwmfViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMapFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog() { Multiselect = false };
+            var result = dialog.ShowDialog();
+            if (result == true)
+            {
+                App.Current.MainWindow.Title = $"Tiledriver UWMF Viewer - {dialog.SafeFileName}";
+            }
+        }
+
+        private void QuitApplication(object sender, System.Windows.RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
