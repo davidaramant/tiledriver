a806d3996e4d56dd8f431921242c9a5e8963357f
David Aramant <davidaramant@gmail.com>
2016-07-24 10:25:26 -0400

    Classes for reading out GameMaps format

---
+++ Tiledriver/Tiledriver.Core/GameMaps/OffsetData.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2016 David Aramant
+// Distributed under the GNU GPL v2. For full terms see the file LICENSE.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tiledriver.Core.GameMaps
+{
+    public sealed class OffsetData
+    {
+        public const int MaxLevelOffsets = 100;
+
+        public ushort RlewMarker { get; }
+        public IReadOnlyList<uint> Offsets { get; }
+
+        public OffsetData(ushort rlewMarker, IEnumerable<uint> offsets)
+        {
+            RlewMarker = rlewMarker;
+            Offsets = offsets.TakeWhile(offset => offset != 0 && offset != uint.MaxValue).ToList();
+        }
+
+        public static OffsetData ReadOffsets(Stream stream)
+        {
+            var buffer = new byte[2 + 4 * MaxLevelOffsets];
+            stream.Read(buffer, 0, buffer.Length);
+            var rlewMarker = BitConverter.ToUInt16(buffer, 0);
+
+            var offsets = new List<uint>();
+
+            for (int i = 0; i < MaxLevelOffsets; i++)
+            {
+                offsets.Add(BitConverter.ToUInt32(buffer, 2 + 4 * i));
+            }
+
+            return new OffsetData(rlewMarker: rlewMarker, offsets: offsets);
+        }
+    }
+}
