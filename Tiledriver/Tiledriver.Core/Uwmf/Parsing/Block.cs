6a817e3c243f0b8158633465e6bd1806f15727d5
David Aramant <davidaramant@gmail.com>
2016-08-20 17:40:13 -0400

    Adding syntax data model

---
+++ Tiledriver/Tiledriver.Core/Uwmf/Parsing/Block.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2016, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Tiledriver.Core.Uwmf.Parsing
+{
+    [DebuggerDisplay("{ToString()}")]
+    public sealed class Block : IEnumerable<Assignment>
+    {
+        private readonly List<Assignment> _properties;
+        public Identifier Name { get; }
+
+        public Block(Identifier name, IEnumerable<Assignment> propertyAssignments)
+        {
+            Name = name;
+            _properties = propertyAssignments.ToList();
+
+            if (_properties.Select(p => p.Name).Distinct().Count() != _properties.Count)
+            {
+                throw new ArgumentException($"Duplicate property assignments found in {Name}", nameof(propertyAssignments));
+            }
+        }
+
+        public IEnumerator<Assignment> GetEnumerator()
+        {
+            return _properties.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString() => Name + "\n" + string.Join("\n", _properties.Select(u => $"* {u}"));
+    }
+}
---
+++ Tiledriver/Tiledriver.Core/Uwmf/Parsing/Token.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) 2016, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.Diagnostics;
+
+namespace Tiledriver.Core.Uwmf.Parsing
+{
+    public enum TokenType
+    {
+        Identifier,
+        Equal,
+        Semicolon,
+        String,
+        Integer,
+        Double,
+        BooleanTrue,
+        BooleanFalse,
+        OpenParen,
+        CloseParen,
+    }
+
+    [DebuggerDisplay("{ToString()}")]
+    public sealed class Token
+    {
+        private readonly object _value;
+        public TokenType Type { get; }
+
+        private Token(TokenType type, object value)
+        {
+            Type = type;
+            _value = value;
+        }
+
+        public bool IsValue =>
+            Type == TokenType.BooleanFalse ||
+            Type == TokenType.BooleanTrue ||
+            Type == TokenType.Double ||
+            Type == TokenType.Integer ||
+            Type == TokenType.String;
+
+        public string ValueAsString
+        {
+            get
+            {
+                if (!(Type == TokenType.String || Type == TokenType.Identifier))
+                {
+                    throw new InvalidOperationException($"Tried to access a {Type} token value as a string.");
+                }
+                return (string)_value;
+            }
+        }
+
+        public int ValueAsInt
+        {
+            get
+            {
+                if (Type != TokenType.Integer)
+                {
+                    throw new InvalidOperationException($"Tried to access a {Type} token value as an integer.");
+                }
+                return (int)_value;
+            }
+        }
+
+        public double ValueAsDouble
+        {
+            get
+            {
+                if (Type != TokenType.Double)
+                {
+                    throw new InvalidOperationException($"Tried to access a {Type} token value as a double.");
+                }
+                return (double)_value;
+            }
+        }
+
+        public bool ValueAsBool
+        {
+            get
+            {
+                if (!(Type == TokenType.BooleanTrue || Type == TokenType.BooleanFalse))
+                {
+                    throw new InvalidOperationException($"Tried to access a {Type} token value as a boolean.");
+                }
+                return (bool)_value;
+            }
+        }
+
+        public override string ToString()
+        {
+            switch (Type)
+            {
+                case TokenType.Identifier:
+                case TokenType.String:
+                case TokenType.Integer:
+                case TokenType.Double:
+                    return $"{Type}: {_value}";
+
+                default:
+                    return Type.ToString();
+            }
+        }
+
+        public static Token Identifier(string id)
+        {
+            return new Token(TokenType.Identifier, id);
+        }
+        public static Token String(string value)
+        {
+            return new Token(TokenType.String, value);
+        }
+        public static Token Integer(int value)
+        {
+            return new Token(TokenType.Integer, value);
+        }
+        public static Token Double(double value)
+        {
+            return new Token(TokenType.Double, value);
+        }
+
+        public static readonly Token BooleanTrue = new Token(TokenType.BooleanTrue, null);
+        public static readonly Token BooleanFalse = new Token(TokenType.BooleanFalse, null);
+        public static readonly Token Equal = new Token(TokenType.Equal, null);
+        public static readonly Token Semicolon = new Token(TokenType.Semicolon, null);
+        public static readonly Token OpenParen = new Token(TokenType.OpenParen, null);
+        public static readonly Token CloseParen = new Token(TokenType.CloseParen, null);
+    }
+}
