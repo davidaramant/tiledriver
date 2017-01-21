6c4116397188599b4ac0371b0fc252da3af4cfcb
David Aramant <davidaramant@gmail.com>
2017-01-20 21:53:05 -0500

    empty MapInfo parser framework

---
+++ Tiledriver/Tiledriver.Metadata/MapInfoParserGenerator.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+namespace Tiledriver.Metadata
+{
+    public static class MapInfoParserGenerator
+    {
+        public static string GetText()
+        {
+            return "";
+        }
+    }
+}
