ee54f850570f7654d5735ceef166c6f9d65dcb8e
Leon Organ <lrorgan@sep.com>
2017-08-05 10:48:03 -0400

    Framework for map abstraction

---
+++ Tiledriver/Tiledriver.Core/LevelGeometry/Mapping/IRoom.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2017, Leon Organ
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Collections.Generic;
+
+namespace Tiledriver.Core.LevelGeometry.Mapping
+{
+    public interface IRoom
+    {
+        IDictionary<IPassage, IRoom> AdjacentRooms { get; }
+    }
+}
