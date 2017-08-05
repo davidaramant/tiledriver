933f498758373cca3bfc27ebdc00881f367dd8b1
Aaron Alexander <ajalexander.dev@gmail.com>
2017-08-04 20:46:53 -0400

    Adding first validity rule

---
+++ Tiledriver/Tiledriver.Core/MapRanker/IRule.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2017, Aaron Alexander
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using Tiledriver.Core.FormatModels.Uwmf;
+
+namespace Tiledriver.Core.MapRanker
+{
+    public interface IRule
+    {
+        bool Passes(MapData data);
+    }
+}
