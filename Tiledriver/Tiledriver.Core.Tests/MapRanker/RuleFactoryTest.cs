6028f939374c26d82daebe488e30066a2afb48d1
Aaron Alexander <ajalexander.dev@gmail.com>
2017-08-05 11:03:08 -0400

    MechaHitler should also be a valid exit position

--- Tiledriver/Tiledriver.Core.Tests/MapRanker/RuleFactoryTest.cs
+++ Tiledriver/Tiledriver.Core.Tests/MapRanker/RuleFactoryTest.cs
@@ -23,9 +23,10 @@ namespace Tiledriver.Core.Tests.MapRanker
         {
             var rules = _target.Rules.ToList();
 
-            Assert.That(rules.Count, Is.EqualTo(2));
+            Assert.That(rules.Count, Is.EqualTo(3));
             Assert.That(rules[0], Is.InstanceOf<HasStartPosition>());
             Assert.That(rules[1], Is.InstanceOf<StartPositionValid>());
+            Assert.That(rules[2], Is.InstanceOf<HasExit>());
         }
     }
 }
