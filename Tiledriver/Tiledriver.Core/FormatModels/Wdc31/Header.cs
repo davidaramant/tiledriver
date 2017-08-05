c80916591193af4af55f4b9a4497cfecfaccec35
David Aramant <davidaramant@gmail.com>
2017-08-05 13:30:51 -0400

    WIP on WDC3.1 format

---
+++ Tiledriver/Tiledriver.Core/FormatModels/Wdc31/Header.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+namespace Tiledriver.Core.FormatModels.Wdc31
+{
+    public sealed class Header
+    {
+        public Header(
+            string fileVersion, 
+            long numberOfMaps, 
+            int numberOfMapPlanes, 
+            int maxMapNameLength)
+        {
+            FileVersion = fileVersion;
+            NumberOfMaps = numberOfMaps;
+            NumberOfMapPlanes = numberOfMapPlanes;
+            MaxMapNameLength = maxMapNameLength;
+        }
+
+        public string FileVersion { get; }
+        public long NumberOfMaps { get; }
+        public int NumberOfMapPlanes { get; }
+        public int MaxMapNameLength { get; }
+    }
+}
---
+++ Tiledriver/Tiledriver.Core/FormatModels/Wdc31/Wdc31Bundle.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.IO;
+
+namespace Tiledriver.Core.FormatModels.Wdc31
+{
+    public sealed class Wdc31Bundle
+    {
+        private const string FileVersion = "WDC3.1";
+        private readonly Header _header;
+
+        public static Wdc31Bundle Load(Stream mapStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
