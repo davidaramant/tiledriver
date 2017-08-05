2c8a0a181772a4d8eb62923d66de03eac0e81616
David Aramant <davidaramant@gmail.com>
2017-08-05 13:30:51 -0400

    WIP on WDC3.1 format

---
+++ Tiledriver/Tiledriver.Core/FormatModels/Wdc31/MapInfo.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Collections.Immutable;
+
+namespace Tiledriver.Core.FormatModels.Wdc31
+{
+    public sealed class MapInfo
+    {
+        public string MapName { get; }
+        public int MapWidth { get; }
+        public int MapHeight { get; }
+        public ImmutableArray<int> MapData { get; }
+
+        public MapInfo(
+            string mapName,
+            int mapWidth,
+            int mapHeight,
+            ImmutableArray<int> mapData)
+        {
+            MapName = mapName;
+            MapWidth = mapWidth;
+            MapHeight = mapHeight;
+            MapData = mapData;
+        }
+    }
+}
