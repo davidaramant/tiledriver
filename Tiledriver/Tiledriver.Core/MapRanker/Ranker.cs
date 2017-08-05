e333c2ed4db610054814f13287d370eb71ab0a8d
Aaron Alexander <ajalexander.dev@gmail.com>
2017-08-04 20:46:53 -0400

    Adding first validity rule

---
+++ Tiledriver/Tiledriver.Core/MapRanker/Ranker.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2017, Aaron Alexander
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using Tiledriver.Core.FormatModels.Uwmf;
+
+namespace Tiledriver.Core.MapRanker
+{
+    public class Ranker
+    {
+        private readonly  IRuleFactory _factory;
+
+
+        public Ranker()
+            : this(new RuleFactory())
+        {
+        }
+
+        public Ranker(IRuleFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public int RankLevel(MapData data)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Tiledriver/Tiledriver.Core/MapRanker/RuleFactory.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2017, Aaron Alexander
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Collections.Generic;
+
+namespace Tiledriver.Core.MapRanker
+{
+    public class RuleFactory : IRuleFactory
+    {
+        public IEnumerable<IRule> Rules
+        {
+            get
+            {
+                yield return new HasStartPosition();
+            }
+        }
+    }
+}
