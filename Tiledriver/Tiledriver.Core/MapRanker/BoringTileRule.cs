b3f896d61b673e3b02ad1eb2696abf9efb77bde6
Leon Organ <lrorgan@sep.com>
2017-08-06 14:14:46 -0400

    fix boring check

--- Tiledriver/Tiledriver.Core/MapRanker/BoringTileRule.cs
+++ Tiledriver/Tiledriver.Core/MapRanker/BoringTileRule.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2017, Aaron Alexander
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
+using System;
 using System.Linq;
 using Tiledriver.Core.FormatModels.Uwmf;
 using Tiledriver.Core.LevelGeometry.Mapping;
@@ -12,18 +13,21 @@ namespace Tiledriver.Core.MapRanker
     /// </summary>
     public class BoringTileRule : IRankingRule
     {
-        private const double AcceptablePercentage = 0.65;
+        private const double AcceptablePercentage = 0.70;
 
         public int Rank(MapData data, LevelMap levelMap)
         {
             var allLocations = levelMap.AllRooms.SelectMany(room => room.Locations).Count();
             var boringLocations = levelMap.AllRooms.Sum(room => room.BoringTiles);
 
-            var boringPercentage = boringLocations / allLocations;
+            var boringPercentage = boringLocations / (double)allLocations;
+
+            Console.WriteLine("Boring Percent: {0}", boringPercentage);
+
             if (boringPercentage <= AcceptablePercentage)
                 return 0;
 
-            return (int) ((boringPercentage - AcceptablePercentage) * 100);
+            return (int) ((boringPercentage - AcceptablePercentage) * -100);
         }
     }
 }
