d589a18a9f1e0579933a8bef312eef40204ec7f0
David Aramant <davidaramant@gmail.com>
2017-08-05 21:26:24 -0400

    Added CompoundResourceProvider

--- Tiledriver/Tiledriver.Core/FormatModels/Pk3/Pk3File.cs
+++ Tiledriver/Tiledriver.Core/FormatModels/Pk3/Pk3File.cs
@@ -5,20 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Functional.Maybe;
 using SharpCompress.Archives.Zip;
 using SharpCompress.Readers;
 
 namespace Tiledriver.Core.FormatModels.Pk3
 {
-    public sealed class Pk3File : IResourceProvider, IDisposable
+    public sealed class Pk3File : IResourceProvider
     {
-        private readonly string _fileName;
         private readonly ZipArchive _archive;
 
         private Pk3File(string fileName)
         {
-            _fileName = fileName;
-            _archive = ZipArchive.Open(File.OpenRead(_fileName), new ReaderOptions { LeaveStreamOpen = false });
+            _archive = ZipArchive.Open(File.OpenRead(fileName), new ReaderOptions { LeaveStreamOpen = false });
         }
 
         public static Pk3File Open(string fileName)
@@ -28,17 +27,23 @@ namespace Tiledriver.Core.FormatModels.Pk3
 
         public Stream Lookup(string path)
         {
-            var entry = _archive.Entries.SingleOrDefault(e => !e.IsDirectory &&
-                                                   StringComparer.InvariantCultureIgnoreCase.Equals(path, e.Key));
-            if (entry == null)
-            {
-                throw new EntryNotFoundException(path);
-            }
-
-            return entry.OpenEntryStream();
+            return TryLookup(path).OrElse(() => new EntryNotFoundException(path));
         }
 
-        public IEnumerator<string> GetAllEntryNames()
+        public Maybe<Stream> TryLookup(string path)
+        {
+            var entry = _archive.Entries.SingleOrDefault(e =>
+                !e.IsDirectory && StringComparer.InvariantCultureIgnoreCase.Equals(path, e.Key));
+
+            if (entry == null)
+            {
+                return Maybe<Stream>.Nothing;
+            }
+
+            return entry.OpenEntryStream().ToMaybe();
+        }
+
+        public IEnumerable<string> GetAllEntryNames()
         {
             foreach (var entry in _archive.Entries.Where(entry => !entry.IsDirectory))
             {
