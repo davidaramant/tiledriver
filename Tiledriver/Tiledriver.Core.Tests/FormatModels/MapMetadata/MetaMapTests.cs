0ef4fd3f04983ed7eda0f23437cf263498c38c0c
David Aramant <davidaramant@gmail.com>
2017-12-02 17:38:23 -0500

    Added saving/loading for MetaMap

---
+++ Tiledriver/Tiledriver.Core.Tests/FormatModels/MapMetadata/MetaMapTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.IO;
+using NUnit.Framework;
+using Tiledriver.Core.FormatModels.MapMetadata;
+
+namespace Tiledriver.Core.Tests.FormatModels.MapMetadata
+{
+    [TestFixture]
+    public sealed class MetaMapTests
+    {
+        [Test]
+        public void ShouldRoundTripMetaMap()
+        {
+            var numTypes = Enum.GetValues(typeof(TileType)).Length;
+
+            var m = new MetaMap(4, 5);
+            for (int y = 0; y < m.Height; y++)
+            {
+                for (int x = 0; x < m.Width; x++)
+                {
+                    m[x, y] = (TileType)((y * m.Width + x) % numTypes);
+                }
+            }
+
+            var tempPath = Path.GetTempFileName();
+
+            try
+            {
+                m.Save(tempPath);
+                var roundTripped = MetaMap.Load(tempPath);
+
+                Assert.That(roundTripped.Width, Is.EqualTo(m.Width), "Width");
+                Assert.That(roundTripped.Height, Is.EqualTo(m.Height), "Height");
+                for (int y = 0; y < m.Height; y++)
+                {
+                    for (int x = 0; x < m.Width; x++)
+                    {
+                        Assert.That(roundTripped[x, y], Is.EqualTo(m[x, y]), $"({x},{y})");
+                    }
+                }
+            }
+            finally
+            {
+                if (File.Exists(tempPath))
+                {
+                    File.Delete(tempPath);
+                }
+            }
+        }
+    }
+}
--- Tiledriver/Tiledriver.Core/FormatModels/MapMetadata/MetaMap.cs
+++ Tiledriver/Tiledriver.Core/FormatModels/MapMetadata/MetaMap.cs
@@ -2,11 +2,15 @@
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
 using System.Drawing;
+using System.IO;
+using Tiledriver.Core.FormatModels.Common;
 
 namespace Tiledriver.Core.FormatModels.MapMetadata
 {
     public sealed class MetaMap
     {
+        private const ulong Version = 0x100;
+
         private readonly TileType[] _tiles;
 
         public int Width { get; }
@@ -30,6 +34,51 @@ namespace Tiledriver.Core.FormatModels.MapMetadata
             Width = width;
             Height = height;
             _tiles = new TileType[width * height];
+        }
+
+        public void Save(string path)
+        {
+            using (var fs = File.Open(path, FileMode.Create))
+            using (var writer = new BinaryWriter(fs))
+            {
+                writer.Write(Version);
+                writer.Write(Width);
+                writer.Write(Height);
+
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        writer.Write((byte)this[x, y]);
+                    }
+                }
+            }
+        }
+
+        public static MetaMap Load(string path)
+        {
+            using (var fs = File.Open(path, FileMode.Open))
+            using (var reader = new BinaryReader(fs))
+            {
+                if (reader.ReadUInt64() != Version)
+                {
+                    throw new ParsingException("Unsupported version");
+                }
+                var width = reader.ReadInt32();
+                var height = reader.ReadInt32();
+
+                var map = new MetaMap(width, height);
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        map[x, y] = (TileType) reader.ReadByte();
+                    }
+                }
+
+                return map;
+            }
         }
     }
 }
