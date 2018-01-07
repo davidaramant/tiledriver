1b7d0844f59f792e5f111ac256df14bc1d0b8475
David Aramant <davidaramant@gmail.com>
2018-01-07 17:12:07 -0500

    Added explicit values for enum that is used in MetaMaps

--- Tiledriver/Tiledriver.Core/FormatModels/MapMetadata/TileType.cs
+++ Tiledriver/Tiledriver.Core/FormatModels/MapMetadata/TileType.cs
@@ -5,10 +5,10 @@ namespace Tiledriver.Core.FormatModels.MapMetadata
 {
     public enum TileType : byte
     {
-        Unreachable,
-        Empty,
-        Wall,
-        PushWall,
-        Door,
+        Unreachable = 0,
+        Empty = 1,
+        Wall = 2,
+        PushWall = 3,
+        Door = 4,
     }
 }
