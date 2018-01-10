e371f01f50ba5c419effecd8cb9d335b1addf701
Jeremy Jarvis <vertigon451@fastmail.com>
2018-01-10 12:15:43 -0500

    Tweaked one-hot encoding to emphasize playable space
    Also performed minor refactors/renames for clarity.

--- ml/metamaps.py
+++ ml/metamaps.py
@@ -13,34 +13,35 @@ class TileType(IntEnum):
 
 class EncodingDim(IntEnum):
     """Dimensions for the one-hot encoding of a metamap"""
-    EMPTY = 0
-    WALL = 1
+    PLAYABLE = 0
+    SOLID = 1
     DOOR = 2
 
 def load_metamap(filename):
     """Loads a metamap from a file into a numpy array of shape (width, height, 3)"""
     with open(filename, "rb") as fin:
-        version = struct.unpack('L', fin.read(8))[0]
+        version = struct.unpack('Q', fin.read(8))[0]
 
         if version != 0x100:
             raise ValueError("Unsupported metamap version")
 
         width = struct.unpack("i", fin.read(4))[0]
         height = struct.unpack("i", fin.read(4))[0]
+        size = width * height
 
         raw_map = np.fromfile(fin, dtype=np.uint8)
-        one_hot = np.zeros([raw_map.shape[0], 3])
+        one_hot = np.zeros([size, 3])
 
-        for i in range(raw_map.shape[0]):
+        for i in range(size):
             tile_type = TileType(raw_map[i])
-            if tile_type == TileType.UNREACHABLE or tile_type == TileType.EMPTY:
-                one_hot[i, EncodingDim.EMPTY] = 1
-            elif tile_type == TileType.WALL:
-                one_hot[i, EncodingDim.WALL] = 1
+            if tile_type == TileType.EMPTY:
+                one_hot[i, EncodingDim.PLAYABLE] = 1
+            elif tile_type == TileType.UNREACHABLE or tile_type == TileType.WALL:
+                one_hot[i, EncodingDim.SOLID] = 1
             elif tile_type == TileType.PUSHWALL or tile_type == TileType.DOOR:
                 one_hot[i, EncodingDim.DOOR] = 1
 
-        one_hot.shape = (width, height, 3)
+        one_hot.shape = (width, height, len(EncodingDim))
 
         return one_hot
 
