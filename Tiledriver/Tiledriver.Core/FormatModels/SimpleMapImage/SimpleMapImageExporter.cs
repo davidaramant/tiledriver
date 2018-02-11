305310ac243eedf14b2ad4b6a44eaeed0a1f5a4e
David Aramant <davidaramant@gmail.com>
2018-02-10 20:47:42 -0500

    Export room graph as picture

--- Tiledriver/Tiledriver.Core/FormatModels/SimpleMapImage/SimpleMapImageExporter.cs
+++ Tiledriver/Tiledriver.Core/FormatModels/SimpleMapImage/SimpleMapImageExporter.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) 2017, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
+using System.Drawing;
+using System.Linq;
+using ColorMine.ColorSpaces;
 using Tiledriver.Core.FormatModels.MapMetadata;
 using Tiledriver.Core.Utils;
 
@@ -10,7 +13,7 @@ namespace Tiledriver.Core.FormatModels.SimpleMapImage
     {
         public static void Export(MetaMap map, MapPalette palette, string outputFilePath, uint scale = 1)
         {
-            var image = new ScaledFastImage(map.Width,map.Height,scale);
+            var image = new ScaledFastImage(map.Width, map.Height, scale);
             for (var tileY = 0; tileY < map.Height; tileY++)
             {
                 for (var tileX = 0; tileX < map.Width; tileX++)
@@ -19,6 +22,29 @@ namespace Tiledriver.Core.FormatModels.SimpleMapImage
                     image.SetPixel(tileX, tileY, tileColor);
                 }
             }
+            image.Save(outputFilePath);
+        }
+
+        public static void Export(RoomGraph graph, string outputFilePath, uint scale = 1)
+        {
+            var image = new ScaledFastImage(graph.Width, graph.Height, scale);
+            image.Fill(Color.Black);
+
+            var increment = 360.0 / graph.RoomCount;
+
+            foreach (var (room, index) in graph.Select((room, index) => (room, index)))
+            {
+                var hue = index * increment;
+                var hsvColor = new Hsv(hue, 1, 1);
+                var rgb = hsvColor.ToRgb();
+                var color = Color.FromArgb((int)rgb.R, (int)rgb.G, (int)rgb.B);
+
+                foreach (var spot in room)
+                {
+                    image.SetPixel(spot, color);
+                }
+            }
+
             image.Save(outputFilePath);
         }
     }
