dcde4e4c25f2576c5f466782dd728a69d9c2ef3c
David Aramant <davidaramant@gmail.com>
2021-04-03 11:15:42 -0400

    Fixing namespace on something

--- Tiledriver/Tiledriver.DataModelGenerator/Utilities/CurrentLibraryInfo.cs
+++ Tiledriver/Tiledriver.DataModelGenerator/Utilities/CurrentLibraryInfo.cs
@@ -4,7 +4,7 @@
 using System;
 using System.Reflection;
 
-namespace SectorDirector.DataModelGenerator.Utilities
+namespace Tiledriver.DataModelGenerator.Utilities
 {
     public static class CurrentLibraryInfo
     {
