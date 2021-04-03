7f73a020a77e913dc0dc0802419632f9dcf4f273
David Aramant <davidaramant@gmail.com>
2021-04-03 11:17:16 -0400

    Rename source directory

--- src/Tiledriver.DataModelGenerator/Utilities/IndentedWriter.cs
+++ src/Tiledriver.DataModelGenerator/Utilities/IndentedWriter.cs
@@ -4,7 +4,7 @@
 using System;
 using System.IO;
 
-namespace SectorDirector.DataModelGenerator.Utilities
+namespace Tiledriver.DataModelGenerator.Utilities
 {
     public sealed class IndentedWriter : IDisposable
     {
