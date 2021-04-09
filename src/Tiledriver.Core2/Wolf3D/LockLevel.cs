649660817e9a4e98743576fd7dd30dc639471c72
David Aramant <davidaramant@gmail.com>
2021-04-08 22:06:12 -0400

    Ported over Wolf3D core namespace

---
+++ src/Tiledriver.Core2/Wolf3D/LockLevel.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2017, Leon Organ
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
+
+namespace Tiledriver.Core.Wolf3D
+{
+    public enum LockLevel
+    {
+        None = 0,
+        Gold = 1,
+        Silver = 2,
+        Both = 101
+    }
+}
