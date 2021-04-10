d228ce7f64499bab98dafde2a2bf955c0a4c85ab
David Aramant <davidaramant@gmail.com>
2021-04-10 14:07:08 -0400

    ported over Sector Director AST

---
+++ src/Tiledriver.Core2/FormatModels/Uwmf/Reading/AbstractSyntaxTree/Assignment.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2019, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using Tiledriver.Core.FormatModels.Common;
+using System.Diagnostics;
+
+namespace Tiledriver.Core.FormatModels.Uwmf.Reading.AbstractSyntaxTree
+{
+    [DebuggerDisplay("{ToString()}")]
+    public sealed record Assignment(Identifier Name, Token Value) : IGlobalExpression
+    {
+        public string ValueAsString() => Value switch
+        {
+            IntegerToken i => i.Value.ToString(),
+            FloatToken f => ToStringWithDecimal(f.Value),
+            BooleanToken b => b.Value.ToString().ToLowerInvariant(),
+            StringToken s => '"' + s.Value + '"',
+            _ => Value.ToString()
+        };
+
+        public override string ToString() => $"{Name}: {ValueAsString()} ({Value.GetType()})";
+
+        private static string ToStringWithDecimal(double source) => (source % 1) == 0 ? source.ToString("f1") : source.ToString();
+    }
+}
---
+++ src/Tiledriver.Core2/FormatModels/Uwmf/Reading/AbstractSyntaxTree/Block.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2019, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using Tiledriver.Core.FormatModels.Common;
+using System.Collections.Immutable;
+
+namespace Tiledriver.Core.FormatModels.Uwmf.Reading.AbstractSyntaxTree
+{
+    public sealed record Block(Identifier Name, ImmutableArray<Assignment> Fields) : IGlobalExpression;
+}
