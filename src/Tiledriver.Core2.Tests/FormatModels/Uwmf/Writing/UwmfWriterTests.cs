ef597e668ab376c761c792decd3b41ad8f7ab734
David Aramant <davidaramant@gmail.com>
2021-04-11 12:29:39 -0400

    Fixed system newline issue

--- src/Tiledriver.Core2.Tests/FormatModels/Uwmf/Writing/UwmfWriterTests.cs
+++ src/Tiledriver.Core2.Tests/FormatModels/Uwmf/Writing/UwmfWriterTests.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) 2021, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
-
+using FluentAssertions;
 using System.Collections.Immutable;
 using System.IO;
 using System.Text;
-using FluentAssertions;
 using Tiledriver.Core.FormatModels.Uwmf;
 using Tiledriver.Core.FormatModels.Uwmf.Writing;
 using Xunit;
@@ -18,11 +17,11 @@ namespace Tiledriver.Core.Tests.FormatModels.Uwmf.Writing
         public void ShouldFormatMapNicely()
         {
             var map = new MapData(
-                NameSpace: "Wolf 3D", 
-                TileSize: 64, 
+                NameSpace: "Wolf 3D",
+                TileSize: 64,
                 Name: "Some Name",
-                Width: 2, 
-                Height: 1, 
+                Width: 2,
+                Height: 1,
                 Tiles: ImmutableList.Create<Tile>().Add(new Tile(
                     TextureEast: "east",
                     TextureNorth: "north",
@@ -33,8 +32,8 @@ namespace Tiledriver.Core.Tests.FormatModels.Uwmf.Writing
                 Planes: ImmutableList<Plane>.Empty,
                 PlaneMaps: ImmutableList.Create<PlaneMap>().Add(new PlaneMap(
                     ImmutableList.Create<TileSpace>()
-                        .Add(new TileSpace(1,2,3))
-                        .Add(new TileSpace(4,5,6,7)))),
+                        .Add(new TileSpace(1, 2, 3))
+                        .Add(new TileSpace(4, 5, 6, 7)))),
                 Things: ImmutableList<Thing>.Empty,
                 Triggers: ImmutableList<Trigger>.Empty);
 
@@ -45,26 +44,34 @@ namespace Tiledriver.Core.Tests.FormatModels.Uwmf.Writing
             using var reader = new StreamReader(ms, Encoding.ASCII);
             var actual = reader.ReadToEnd();
 
-            string expected = 
-                "namespace = \"Wolf 3D\";\r\n" +
-                "tileSize = 64;\r\n" +
-                "name = \"Some Name\";\r\n" +
-                "width = 2;\r\n" +
-                "height = 1;\r\n" +
-                "tile\r\n" +
-                "{\r\n" +
-                "\ttextureEast = \"east\";\r\n" +
-                "\ttextureNorth = \"north\";\r\n" +
-                "\ttextureWest = \"west\";\r\n" +
-                "\ttextureSouth = \"south\";\r\n" +
-                "}\r\n" +
-                "planeMap\r\n" +
-                "{\r\n" +
-                "\t{1,2,3},\r\n" +
-                "\t{4,5,6,7}\r\n" +
-                "}\r\n";
+            var lines =
+                new[]{
+                    "namespace = \"Wolf 3D\";",
+                    "tileSize = 64;",
+                    "name = \"Some Name\";",
+                    "width = 2;",
+                    "height = 1;",
+                    "tile",
+                    "{",
+                    "\ttextureEast = \"east\";",
+                    "\ttextureNorth = \"north\";",
+                    "\ttextureWest = \"west\";",
+                    "\ttextureSouth = \"south\";",
+                    "}",
+                    "planeMap",
+                    "{",
+                    "\t{1,2,3},",
+                    "\t{4,5,6,7}",
+                    "}"
+                };
 
-            actual.Should().Be(expected);
+            var sb = new StringBuilder();
+            foreach (var line in lines)
+            {
+                sb.AppendLine(line);
+            }
+
+            actual.Should().Be(sb.ToString());
         }
     }
 }
