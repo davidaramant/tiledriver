b2baba663e003e5d62877b5a8c2236aa777e4a4b
David Aramant <davidaramant@gmail.com>
2021-04-18 12:29:25 -0400

    Ported over some resource provider stuff

---
+++ src/Tiledriver.Core2/FormatModels/CompoundResourceProvider.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tiledriver.Core.FormatModels
+{
+    public sealed class CompoundResourceProvider : IResourceProvider
+    {
+        private readonly Stack<IResourceProvider> _providers = new Stack<IResourceProvider>();
+
+        public void AddProvider(IResourceProvider provider)
+        {
+            _providers.Push(provider);
+        }
+
+        public Stream Lookup(string path)
+        {
+            return TryLookup(path) ?? throw new EntryNotFoundException(path);
+        }
+
+        public Stream? TryLookup(string path)
+        {
+            foreach (var provider in _providers)
+            {
+                var result = provider.TryLookup(path);
+                if (result!=null)
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        public void Dispose()
+        {
+            foreach (var provider in _providers)
+            {
+                provider.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Tiledriver.Core2/FormatModels/IResourceProvider.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.IO;
+
+namespace Tiledriver.Core.FormatModels
+{
+    public interface IResourceProvider : IDisposable
+    {
+        /// <summary>
+        /// Looks up a resource path.
+        /// </summary>
+        /// <param name="path">The path to the resource.</param>
+        /// <returns>The stream for the resource.</returns>
+        /// <exception cref="EntryNotFoundException"/>
+        Stream Lookup(string path);
+
+        Stream? TryLookup(string path);
+    }
+}
