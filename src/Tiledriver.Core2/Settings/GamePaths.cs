60b500fa30a822aa31213a80424d8e06e7ab6cd5
David Aramant <davidaramant@gmail.com>
2021-04-18 12:20:36 -0400

    Ported over GamePaths

---
+++ src/Tiledriver.Core2/Settings/GamePaths.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+namespace Tiledriver.Core.Settings
+{
+    public sealed record GamePaths(string? Wolf3D, string? SpearOfDestiny);
+}
