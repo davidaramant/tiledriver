34400da79e116355306e9dc409215b83e9e9fbc6
David Aramant <davidaramant@gmail.com>
2021-04-24 10:59:48 -0400

    Better tests for XLAT lexing

--- src/Tiledriver.Core2.Tests/FormatModels/Common/Reading/UnifiedLexerTests.cs
+++ src/Tiledriver.Core2.Tests/FormatModels/Common/Reading/UnifiedLexerTests.cs
@@ -192,11 +192,20 @@ namespace Tiledriver.Core.Tests.FormatModels.Common.Reading
         }
 
         [Fact]
-        public void ShouldLexXlatTestFile()
+        public void ShouldLexWolfXlatTestFile()
+        {
+            using var stream = TestFile.Xlat.wolf3d;
+            using var textReader = new StreamReader(stream, Encoding.ASCII);
+            var lexer = new UnifiedLexer(textReader, allowDollarIdentifiers: true, allowPipes: true);
+            var result = lexer.Scan().ToArray();
+        }
+
+        [Fact]
+        public void ShouldLexSpearXlatTestFile()
         {
             using var stream = TestFile.Xlat.spear;
             using var textReader = new StreamReader(stream, Encoding.ASCII);
-            var lexer = new UnifiedLexer(textReader);
+            var lexer = new UnifiedLexer(textReader, allowDollarIdentifiers: true, allowPipes: true);
             var result = lexer.Scan().ToArray();
         }
     }
