8244762e42c0d1982fce928014f45dfb7ea4fa35
David Aramant <davidaramant@gmail.com>
2021-04-24 19:07:29 -0400

    Moved something field extensions to better place

--- src/Tiledriver.Core2/FormatModels/Common/Reading/FieldAssignmentExtensions.cs
+++ src/Tiledriver.Core2/FormatModels/Common/Reading/FieldAssignmentExtensions.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) 2021, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
 
-
 using System.Collections.Generic;
 
-namespace Tiledriver.Core.FormatModels.Common.Reading.AbstractSyntaxTree
+namespace Tiledriver.Core.FormatModels.Common.Reading
 {
     public static class FieldAssignmentExtensions
     {
