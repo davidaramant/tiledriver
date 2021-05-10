48013d602e46e21f529b806db8d97b14d91727c0
David Aramant <davidaramant@gmail.com>
2021-05-10 09:15:14 -0400

    Ported over some utilities

---
+++ src/Tiledriver.Core2/Utils/PathUtil.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2017, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Linq;
+using System.IO;
+
+namespace Tiledriver.Utils
+{
+    public static class PathUtil
+    {
+        public static string? VerifyPathExists(string? path) =>
+            Directory.Exists(path) ? path : null;
+
+        public static string? Combine(params string?[] paths) =>
+            paths.Any(p => p is null) ? null : Path.Combine(paths.Select(p => p!).ToArray());
+    }
+}
