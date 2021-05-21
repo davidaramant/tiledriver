79536332aa12c4943e4d368c270aa7af4afe204b
David Aramant <davidaramant@gmail.com>
2021-05-21 16:43:01 -0400

    Simple test for rendered texture

---
+++ src/Tiledriver.Core2.Tests/FormatModels/Textures/RenderedTextureTests.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2021, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
+
+using System.Drawing;
+using System.IO;
+using FluentAssertions;
+using Tiledriver.Core.FormatModels.Textures;
+using Xunit;
+
+namespace Tiledriver.Core.Tests.FormatModels.Textures
+{
+    public sealed class RenderedTextureTests
+    {
+        [Fact]
+        public void ShouldRenderTextureToStream()
+        {
+            using var ms = new MemoryStream();
+            var texture = new RenderedTexture(BackgroundColor: Color.Black, Text: "Test");
+            texture.RenderTo(ms);
+            ms.Length.Should().BeGreaterThan(0);
+        }
+    }
+}
