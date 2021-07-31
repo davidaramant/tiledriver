d1923fcbf1894d57fb21246e1f2e67afb7720842
David Aramant <davidaramant@gmail.com>
2021-07-31 10:30:06 -0400

    Visualizer and WIP on getting the light map to work

---
+++ src/Tiledriver.Core.Tests/LevelGeometry/Lighting/LightTracerManualTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2021, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System;
+using System.IO;
+using Tiledriver.Core.FormatModels.Uwmf;
+using Tiledriver.Core.LevelGeometry;
+using Tiledriver.Core.LevelGeometry.Lighting;
+using Xunit;
+
+namespace Tiledriver.Core.Tests.LevelGeometry.Lighting
+{
+    public sealed class LightTracerManualTests
+    {
+        //[Fact]
+        public void ShouldGenerateVisualizationOfSimpleLightMap()
+        {
+            MapData map = DemoMaps.TileDemoMap.Create();
+
+            var (floorLights, ceilingLights) =
+                LightTracer.Trace(
+                    map,
+                    new LightRange(DarkLevels: 10, LightLevels: 10),
+                    new LightDefinition[]
+                    {
+                        new LightDefinition(new Position(1,1),Brightness:1,Radius:1)
+                    });
+
+            var image = LightMapVisualizer.Render(floorLights);
+            image.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "output.png"));
+        }
+    }
+}
---
+++ src/Tiledriver.Core/LevelGeometry/Lighting/LightMapVisualizer.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2021, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using System.Threading.Tasks;
+using SkiaSharp;
+using Tiledriver.Core.Utils.Images;
+
+namespace Tiledriver.Core.LevelGeometry.Lighting
+{
+    public static class LightMapVisualizer
+    {
+        public static IFastImage Render(LightMap lightMap, uint scale = 4)
+        {
+            var image = new ScaledFastImage(lightMap.Size.Width, lightMap.Size.Height, scale);
+
+            var darkIncrement = 128 / lightMap.Range.DarkLevels;
+            var lightIncrement = 128 / lightMap.Range.LightLevels;
+
+            Parallel.For(0, lightMap.Size.Height, y =>
+            {
+                for (int x = 0; x < lightMap.Size.Width; x++)
+                {
+                    byte intensity = 128;
+                    var light = lightMap[x, y];
+                    if (light > 0)
+                    {
+                        intensity += (byte)(light * lightIncrement);
+                    }
+                    else
+                    {
+                        intensity -= (byte)(light * darkIncrement);
+
+                    }
+                    image.SetPixel(x,y,new SKColor(intensity,intensity,intensity));
+                }
+            });
+
+            return image;
+        }
+    }
+}
