807b31cb31e2f32e003a1800cf235ae2df2b9f4a
David Aramant <davidaramant@gmail.com>
2021-07-31 13:26:53 -0400

    Benchmarks for FastImage

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) 2021, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE. 
+
+using BenchmarkDotNet.Running;
+using Benchmarks;
+
+var summary = BenchmarkRunner.Run<FastImageVsScaledImageBenchmarks>();
