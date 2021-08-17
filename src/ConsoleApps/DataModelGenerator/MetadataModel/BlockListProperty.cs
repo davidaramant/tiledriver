4201d05f921dcf3aa77759c2c47d85716cb6830f
David Aramant <davidaramant@gmail.com>
2021-08-16 22:24:54 -0400

    Port of UDMF definitions

--- src/ConsoleApps/DataModelGenerator/MetadataModel/BlockListProperty.cs
+++ src/ConsoleApps/DataModelGenerator/MetadataModel/BlockListProperty.cs
@@ -1,15 +1,13 @@
 ﻿// Copyright (c) 2021, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
 
-using Humanizer;
-
 namespace Tiledriver.DataModelGenerator.MetadataModel
 {
     sealed class BlockListProperty : CollectionProperty
     {
-        public override string PropertyType => $"ImmutableList<{Name.Pascalize()}>";
+        public override string PropertyType => $"ImmutableList<{ElementTypeName}>";
 
-        public BlockListProperty(string name) : base(name)
+        public BlockListProperty(string name, string? elementType = null) : base(name, elementType)
         {
         }
     }
--- src/ConsoleApps/DataModelGenerator/MetadataModel/CollectionProperty.cs
+++ src/ConsoleApps/DataModelGenerator/MetadataModel/CollectionProperty.cs
@@ -7,11 +7,13 @@ namespace Tiledriver.DataModelGenerator.MetadataModel
 {
     abstract class CollectionProperty : Property
     {
-        public virtual string ElementTypeName => Name.Pascalize();
+        private readonly string? _explicitElementType;
+        public virtual string ElementTypeName => (_explicitElementType ?? Name).Pascalize();
         public override string PropertyName => Name.Pluralize().Pascalize();
 
-        protected CollectionProperty(string name) : base(name)
+        protected CollectionProperty(string name, string? elementTypeName = null) : base(name)
         {
+            _explicitElementType = elementTypeName;
         }
     }
 }
