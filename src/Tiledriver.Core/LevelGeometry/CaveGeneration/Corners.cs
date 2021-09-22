a9e7556f2f8f4c134862b8032be3da950d87dae5
David Aramant <davidaramant@gmail.com>
2021-09-21 20:27:49 -0400

    Named all corner combos

--- src/Tiledriver.Core/LevelGeometry/CaveGeneration/Corners.cs
+++ src/Tiledriver.Core/LevelGeometry/CaveGeneration/Corners.cs
@@ -8,10 +8,25 @@ namespace Tiledriver.Core.LevelGeometry.CaveGeneration
     [Flags]
     enum Corners : byte
     {
-        None = 0,
         BottomLeft = 0b0001,
         BottomRight = 0b0010,
         TopRight = 0b0100,
         TopLeft = 0b1000,
+
+        None = 0,
+        All = BottomLeft | BottomRight | TopRight | TopLeft,
+
+        Top = TopLeft | TopRight,
+        Bottom = BottomLeft | BottomRight,
+        Left = TopLeft | BottomLeft,
+        Right = TopRight | BottomRight,
+
+        ExceptBottomLeft = BottomRight | TopRight | TopLeft,
+        ExceptBottomRight = BottomLeft | TopRight | TopLeft,
+        ExceptTopLeft = BottomLeft | BottomRight | TopRight,
+        ExceptTopRight = BottomLeft | BottomRight | TopLeft,
+
+        TopLeftAndBottomRight = TopLeft | BottomRight,
+        TopRightAndBottomLeft = TopRight | BottomLeft,
     }
 }
