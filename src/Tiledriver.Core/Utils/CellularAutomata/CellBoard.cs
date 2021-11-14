9a983e5aa548ff871614d59f16ee9d6b9625112b
David Aramant <davidaramant@gmail.com>
2021-11-14 15:04:40 -0500

    Immutable CellBoard

--- src/Tiledriver.Core/Utils/CellularAutomata/CellBoard.cs
+++ src/Tiledriver.Core/Utils/CellularAutomata/CellBoard.cs
@@ -11,24 +11,18 @@ namespace Tiledriver.Core.Utils.CellularAutomata
 {
     public sealed class CellBoard : IBox
     {
-        private readonly CellType[,] _odd;
-        private readonly CellType[,] _even;
+        private readonly CellType[,] _board;
 
         public Size Dimensions { get; }
         public int Width => Dimensions.Width;
         public int Height => Dimensions.Height;
-        public int Generation { get; private set; }
-        public CellType this[Position p] => CurrentBoard[p.Y, p.X];
-
-        private CellType[,] CurrentBoard => Generation % 2 == 0 ? _even : _odd;
-        private CellType[,] PreviousBoard => Generation % 2 == 0 ? _odd : _even;
-
+        public CellType this[Position p] => _board[p.Y, p.X];
+        
         public CellBoard(Size dimensions)
         {
             Dimensions = dimensions;
 
-            _odd = new CellType[dimensions.Height, dimensions.Width];
-            _even = new CellType[dimensions.Height, dimensions.Width];
+            _board = new CellType[dimensions.Height, dimensions.Width];
         }
 
         public CellBoard(Size dimensions, Func<Position,CellType> typeAtPosition) : this(dimensions)
@@ -37,28 +31,23 @@ namespace Tiledriver.Core.Utils.CellularAutomata
             {
                 for (int x = 0; x < dimensions.Width; x++)
                 {
-                    _even[y, x] = typeAtPosition(new Position(x, y));
+                    _board[y, x] = typeAtPosition(new Position(x, y));
                 }
             }
         }
 
-        private CellBoard(Size dimensions, CellType[,] initial) : this(dimensions)
-        {
-            _even = initial;
-        }
+        private CellBoard(Size dimensions, CellType[,] initial) : this(dimensions) => _board = initial;
 
         public CellBoard Quadruple()
         {
             var newSize = Dimensions * 2;
             var initial = new CellType[newSize.Height, newSize.Width];
 
-            var current = CurrentBoard;
-
             for (int y = 0; y < Dimensions.Height; y++)
             {
                 for (int x = 0; x < Dimensions.Width; x++)
                 {
-                    var value = current[y, x];
+                    var value = _board[y, x];
                     initial[y * 2, x * 2] = value;
                     initial[y * 2, x * 2 + 1] = value;
                     initial[y * 2 + 1, x * 2] = value;
@@ -71,17 +60,17 @@ namespace Tiledriver.Core.Utils.CellularAutomata
 
         public CellBoard Fill(Random random, double probabilityAlive)
         {
-            Generation = 0;
+            var initial = new CellType[Dimensions.Height, Dimensions.Width];
 
             for (int row = 0; row < Dimensions.Height; row++)
             {
                 for (int col = 0; col < Dimensions.Width; col++)
                 {
-                    _even[row, col] = random.NextDouble() <= probabilityAlive ? CellType.Alive : CellType.Dead;
+                    initial[row, col] = random.NextDouble() <= probabilityAlive ? CellType.Alive : CellType.Dead;
                 }
             }
 
-            return this;
+            return new CellBoard(Dimensions, initial);
         }
 
         public CellBoard AddNoise(Random random, double percentage)
@@ -89,17 +78,21 @@ namespace Tiledriver.Core.Utils.CellularAutomata
             var allPositions = Dimensions.GetAllPositions().ToList();
             allPositions.Shuffle(random);
 
-            foreach(var pos in allPositions.Take((int)(Dimensions.Area * percentage)))
+            var initial = (CellType[,])_board.Clone();
+
+            foreach (var pos in allPositions.Take((int)(Dimensions.Area * percentage)))
             {
-                var value = CurrentBoard[pos.Y, pos.X];
-                CurrentBoard[pos.Y, pos.X] = value == CellType.Alive ? CellType.Dead : CellType.Alive;
+                var value = _board[pos.Y, pos.X];
+                initial[pos.Y, pos.X] = value == CellType.Alive ? CellType.Dead : CellType.Alive;
             }
 
-            return this;
+            return new CellBoard(Dimensions, initial);
         }
 
         public CellBoard MakeBorderAlive(int thickness)
         {
+            var initial = (CellType[,])_board.Clone();
+
             // This can obviously be improved but who cares; doubtful this will be the long pole in the tent
             for (int row = 0; row < Height; row++)
             {
@@ -107,37 +100,42 @@ namespace Tiledriver.Core.Utils.CellularAutomata
                 {
                     if (thickness > 0 && col < thickness || col > (Width - 1 - thickness) || row < thickness || row > (Height - thickness))
                     {
-                        CurrentBoard[row, col] = CellType.Alive;
+                        initial[row, col] = CellType.Alive;
                     }
                 }
             }
 
-            return this;
+            return new CellBoard(Dimensions, initial);
         }
 
         public CellBoard RunGenerations(int generations, CellRule.IsNextGenAlive? isNextGenAlive = null)
         {
             isNextGenAlive ??= CellRule.FiveOrMoreNeighbors;
 
+            var currentBoard = new CellType[Dimensions.Height, Dimensions.Width];
+            var previousBoard = (CellType[,])_board.Clone();
+
             foreach (var g in Enumerable.Range(1, generations))
             {
-                Generation++;
-
                 for (int row = 0; row < Height; row++)
                 {
                     for (int col = 0; col < Width; col++)
                     {
-                        CurrentBoard[row, col] =
+                        currentBoard[row, col] =
                             isNextGenAlive(
-                                PreviousBoard[row, col], 
-                                radius => CountAliveNeighbors(PreviousBoard, row, col, radius)) 
+                                previousBoard[row, col], 
+                                radius => CountAliveNeighbors(previousBoard, row, col, radius)) 
                             ? CellType.Alive 
                             : CellType.Dead;
                     }
                 }
+
+                var temp = currentBoard;
+                currentBoard = previousBoard;
+                previousBoard = temp;
             }
 
-            return this;
+            return new CellBoard(Dimensions, previousBoard);
         }
 
         private int CountAliveNeighbors(CellType[,] board, int row, int col, int radius)
