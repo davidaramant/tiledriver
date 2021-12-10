5028760d703df7cac899a6f113d657dccaaf7c23
David Aramant <davidaramant@gmail.com>
2021-12-09 22:34:23 -0500

    WIP on parsing Doom actors to generate some code

---
+++ src/ConsoleApps/DataModelGenerator/DoomGameInfo/DoomActorGenerator.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) 2021, David Aramant
+// Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace Tiledriver.DataModelGenerator.DoomGameInfo;
+
+internal static class DoomActorGenerator
+{
+    public static void WriteToPath(string basePath)
+    {
+        if (!Directory.Exists(basePath))
+        {
+            Directory.CreateDirectory(basePath);
+        }
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = "Tiledriver.DataModelGenerator.DoomGameInfo.DoomActors.txt";
+
+        using Stream stream = assembly.GetManifestResourceStream(resourceName) ?? throw new Exception($"Could not find {resourceName}");
+        using StreamReader reader = new(stream);
+
+        var categories = ParseCategories(reader);
+    }
+
+    interface IAssignment { string Name { get; } }
+    sealed record StringAssignment(string Name, string Value) : IAssignment;
+    sealed record IntegerAssignment(string Name, int Value) : IAssignment;
+    sealed record ActorDefinition(int Id, IReadOnlyList<IAssignment> Assignments);
+    sealed record ActorCategory(string Name, IReadOnlyList<IAssignment> GlobalAssignments, IReadOnlyList<ActorDefinition> Actors);
+
+    static IEnumerable<ActorCategory> ParseCategories(StreamReader reader)
+    {
+        throw new NotImplementedException();
+    }
+
+    static class DoomActorParser
+    {
+        static readonly Parser<char, char> LBrace = Char('{');
+        static readonly Parser<char, char> RBrace = Char('}');
+        static readonly Parser<char, char> Quote = Char('"');
+        static readonly Parser<char, char> Equal = Char('=');
+        static readonly Parser<char, char> EqualWhitespace = Equal.Between(SkipWhitespaces);
+        static readonly Parser<char, char> SemiColon = Char(';');
+        static readonly Parser<char, char> SemiColonWhitespace = SemiColon.Between(SkipWhitespaces);
+
+        private static readonly Parser<char, string> String =
+            Token(c => c != '"')
+            .ManyString()
+            .Between(Quote);
+        private static readonly Parser<char, int> Integer =
+            Token(char.IsDigit).ManyString().Select(int.Parse);
+
+        static readonly Parser<char, string> Identifier = Token(char.IsLower).ManyString();
+
+        static readonly Parser<char, IAssignment> StringAssignment =
+            from id in Identifier
+            from eq in EqualWhitespace
+            from value in String
+            from sc in SemiColonWhitespace
+            select (IAssignment)(new StringAssignment(id, value));
+
+        static readonly Parser<char, IAssignment> IntegerAssignment =
+            from id in Identifier
+            from eq in EqualWhitespace
+            from value in Integer
+            from sc in SemiColonWhitespace
+            select (IAssignment)(new IntegerAssignment(id, value));
+
+        
+
+        //private static readonly Parser<char, string> String =
+        //Token(c => c != '"')
+        //    .ManyString()
+        //    .Between(Quote);
+        //private static readonly Parser<char, IJson> JsonString =
+        //    String.Select<IJson>(s => new JsonString(s));
+
+        //private static readonly Parser<char, IJson> Json =
+        //    JsonString.Or(Rec(() => JsonArray!)).Or(Rec(() => JsonObject!));
+
+        //private static readonly Parser<char, IJson> JsonArray =
+        //    Json.Between(SkipWhitespaces)
+        //        .Separated(Comma)
+        //        .Between(LBracket, RBracket)
+        //        .Select<IJson>(els => new JsonArray(els.ToImmutableArray()));
+
+        //private static readonly Parser<char, KeyValuePair<string, IJson>> JsonMember =
+        //    String
+        //        .Before(ColonWhitespace)
+        //        .Then(Json, (name, val) => new KeyValuePair<string, IJson>(name, val));
+
+        //private static readonly Parser<char, IJson> JsonObject =
+        //    JsonMember.Between(SkipWhitespaces)
+        //        .Separated(Comma)
+        //        .Between(LBrace, RBrace)
+        //        .Select<IJson>(kvps => new JsonObject(kvps.ToImmutableDictionary()));
+
+        //public static Result<char, IJson> Parse(string input) => Json.Parse(input);
+    }
+}
--- src/ConsoleApps/DataModelGenerator/Program.cs
+++ src/ConsoleApps/DataModelGenerator/Program.cs
@@ -2,61 +2,66 @@
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
 
 using System.IO;
+using Tiledriver.DataModelGenerator.DoomGameInfo;
 using Tiledriver.DataModelGenerator.MapInfo;
 using Tiledriver.DataModelGenerator.Udmf;
 using Tiledriver.DataModelGenerator.Uwmf;
 using Tiledriver.DataModelGenerator.Xlat;
 
-namespace Tiledriver.DataModelGenerator
+namespace Tiledriver.DataModelGenerator;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var basePath = FindSolutionPath();
+
+        var corePath = Path.Combine(basePath, "Tiledriver.Core");
+        var formatModelsPath = Path.Combine(corePath, "FormatModels");
+
+        var udmfPath = Path.Combine(formatModelsPath, "Udmf");
+        var udmfWritingPath = Path.Combine(udmfPath, "Writing");
+        var udmfReadingPath = Path.Combine(udmfPath, "Reading");
+
+        var uwmfPath = Path.Combine(formatModelsPath, "Uwmf");
+        var uwmfWritingPath = Path.Combine(uwmfPath, "Writing");
+        var uwmfReadingPath = Path.Combine(uwmfPath, "Reading");
+
+        var xlatPath = Path.Combine(formatModelsPath, "Xlat");
+        var xlatReadingPath = Path.Combine(xlatPath, "Reading");
+
+        var mapInfoPath = Path.Combine(formatModelsPath, "MapInfo");
+        var mapInfoReadingPath = Path.Combine(mapInfoPath, "Reading");
+
+        var gameInfoPath = Path.Combine(corePath, "GameInfo");
+        var doomGameInfoPath = Path.Combine(gameInfoPath, "Doom");
+
+        UwmfModelGenerator.WriteToPath(uwmfPath);
+        UwmfWriterGenerator.WriteToPath(uwmfWritingPath);
+        UwmfSemanticAnalyzerGenerator.WriteToPath(uwmfReadingPath);
+
+        UdmfModelGenerator.WriteToPath(udmfPath);
+        UdmfWriterGenerator.WriteToPath(udmfWritingPath);
+        UdmfSemanticAnalyzerGenerator.WriteToPath(udmfReadingPath);
+
+        XlatModelGenerator.WriteToPath(xlatPath);
+        XlatParserGenerator.WriteToPath(xlatReadingPath);
+
+        MapInfoModelGenerator.WriteToPath(mapInfoPath);
+        MapInfoParserGenerator.WriteToPath(mapInfoReadingPath);
+
+        DoomActorGenerator.WriteToPath(doomGameInfoPath);
+    }
+
+    private static string FindSolutionPath()
+    {
+        var path = "..";
+
+        while (!File.Exists(Path.Combine(path, "Tiledriver.sln")))
         {
-            var basePath = FindSolutionPath();
-
-            var corePath = Path.Combine(basePath, "Tiledriver.Core");
-            var formatModelsPath = Path.Combine(corePath, "FormatModels");
-            
-            var udmfPath = Path.Combine(formatModelsPath, "Udmf");
-            var udmfWritingPath = Path.Combine(udmfPath, "Writing");
-            var udmfReadingPath = Path.Combine(udmfPath, "Reading");
-
-            var uwmfPath = Path.Combine(formatModelsPath, "Uwmf");
-            var uwmfWritingPath = Path.Combine(uwmfPath, "Writing");
-            var uwmfReadingPath = Path.Combine(uwmfPath, "Reading");
-            
-            var xlatPath = Path.Combine(formatModelsPath, "Xlat");
-            var xlatReadingPath = Path.Combine(xlatPath, "Reading");
-
-            var mapInfoPath = Path.Combine(formatModelsPath, "MapInfo");
-            var mapInfoReadingPath = Path.Combine(mapInfoPath, "Reading");
-
-            UwmfModelGenerator.WriteToPath(uwmfPath);
-            UwmfWriterGenerator.WriteToPath(uwmfWritingPath);
-            UwmfSemanticAnalyzerGenerator.WriteToPath(uwmfReadingPath);
-
-            UdmfModelGenerator.WriteToPath(udmfPath);
-            UdmfWriterGenerator.WriteToPath(udmfWritingPath);
-            UdmfSemanticAnalyzerGenerator.WriteToPath(udmfReadingPath);
-
-            XlatModelGenerator.WriteToPath(xlatPath);
-            XlatParserGenerator.WriteToPath(xlatReadingPath);
-
-            MapInfoModelGenerator.WriteToPath(mapInfoPath);
-            MapInfoParserGenerator.WriteToPath(mapInfoReadingPath);
+            path = Path.Combine(path, "..");
         }
 
-        private static string FindSolutionPath()
-        {
-            var path = "..";
-
-            while (!File.Exists(Path.Combine(path, "Tiledriver.sln")))
-            {
-                path = Path.Combine(path, "..");
-            }
-
-            return Path.GetFullPath(path);
-        }
+        return Path.GetFullPath(path);
     }
 }
