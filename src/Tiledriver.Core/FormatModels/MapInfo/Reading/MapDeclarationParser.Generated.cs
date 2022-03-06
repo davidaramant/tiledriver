95fdc831613f21b1247b32e9e130d2a50a5177c9
David Aramant <davidaramant@gmail.com>
2022-03-06 13:53:16 -0500

    All generated files touched due to Copyright year

--- src/Tiledriver.Core/FormatModels/MapInfo/Reading/MapDeclarationParser.Generated.cs
+++ src/Tiledriver.Core/FormatModels/MapInfo/Reading/MapDeclarationParser.Generated.cs
@@ -1,5 +1,5 @@
 #nullable enable
-// Copyright (c) 2021, David Aramant
+// Copyright (c) 2022, David Aramant
 // Distributed under the 3-clause BSD license.  For full terms see the file LICENSE.
 
 using System.CodeDom.Compiler;
