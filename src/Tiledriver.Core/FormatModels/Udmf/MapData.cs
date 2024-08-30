b38a97304f42cff5dabd1a6df4a6707e307236ea
David Aramant <davidaramant@gmail.com>
2024-08-29 22:54:05 -0400

    Added some useful properties for Doom MapData dimensions

---
+++ src/Tiledriver.Core/FormatModels/Udmf/MapData.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+
+namespace Tiledriver.Core.FormatModels.Udmf;
+
+public sealed partial record MapData
+{
+	public double MinX => Vertices.Min(p => p.X);
+	public double MaxX => Vertices.Max(p => p.X);
+	public double MinY => Vertices.Min(p => p.Y);
+	public double MaxY => Vertices.Max(p => p.Y);
+	public double Width => MaxX - MinX;
+	public double Height => MaxY - MinY;
+}
