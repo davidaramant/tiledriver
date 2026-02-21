356627cedec224d30429fb495d9e477fd3449611
David Aramant <davidaramant@gmail.com>
2026-02-20 19:57:04 -0500

    Found a noise package and made a simpler abstraction for it

---
+++ src/Tiledriver.Core/Utils/Noise/NoiseGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Tiledriver.Core.Utils.Noise;
+
+public interface INoiseGenerator
+{
+	float[,] Generate(int width, int height, float scale, int? seed = null);
+}
+
+public sealed class NoiseGenerator : INoiseGenerator
+{
+	public float[,] Generate(int width, int height, float scale, int? seed = null)
+	{
+		if (seed is not null)
+			SimplexNoise.Noise.Seed = seed.Value;
+
+		return SimplexNoise.Noise.Calc2D(width, height, scale);
+	}
+}
