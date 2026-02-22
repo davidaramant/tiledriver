136b2b844122ac169f009e94cdab728810150e61
David Aramant <davidaramant@gmail.com>
2026-02-22 11:02:31 -0500

    WIP

--- src/Tiledriver.Core.ManualTests/PolygonTerrainVisualization.cs
+++ src/Tiledriver.Core.ManualTests/PolygonTerrainVisualization.cs
@@ -50,26 +50,6 @@ public sealed class PolygonTerrainVisualization() : BaseVisualization("Polygon T
 			using var canvas = new SKCanvas(bitmap);
 			canvas.Clear(SKColors.White);
 
-			// Draw CA board
-			{
-				for (int y = 0; y < caBoard.Height; y++)
-				{
-					for (int x = 0; x < caBoard.Width; x++)
-					{
-						if (caBoard[new Position(x, y)] == CellType.Dead)
-						{
-							canvas.DrawRect(
-								(float)(x * caScale),
-								(float)(y * caScale),
-								(float)caScale,
-								(float)caScale,
-								new SKPaint { Color = SKColors.LightGray, Style = SKPaintStyle.Fill }
-							);
-						}
-					}
-				}
-			}
-
 			// Draw sites
 			foreach (var site in sites)
 			{
@@ -79,16 +59,12 @@ public sealed class PolygonTerrainVisualization() : BaseVisualization("Polygon T
 					var caY = (int)(site.Y / caScale);
 					var siteCenterInDeadCell = caBoard[new Position(caX, caY)] == CellType.Dead;
 
-					if (siteCenterInDeadCell)
-					{
-						var points = site.ClockwisePoints.Select(p => new SKPoint((float)p.X, (float)p.Y)).ToArray();
-						using var path = new SKPath();
-						path.AddPoly(points, close: true);
-						canvas.DrawPath(
-							path,
-							new SKPaint { Color = SKColors.LightGreen.WithAlpha(128), Style = SKPaintStyle.Fill }
-						);
-					}
+					var color = siteCenterInDeadCell ? SKColors.LightGreen : SKColors.RoyalBlue;
+
+					var points = site.ClockwisePoints.Select(p => new SKPoint((float)p.X, (float)p.Y)).ToArray();
+					using var path = new SKPath();
+					path.AddPoly(points, close: true);
+					canvas.DrawPath(path, new SKPaint { Color = color, Style = SKPaintStyle.Fill });
 				}
 
 				canvas.DrawPoint(
@@ -114,8 +90,122 @@ public sealed class PolygonTerrainVisualization() : BaseVisualization("Polygon T
 				);
 			}
 
+			// Draw CA board
+			{
+				for (int y = 0; y < caBoard.Height; y++)
+				{
+					for (int x = 0; x < caBoard.Width; x++)
+					{
+						if (caBoard[new Position(x, y)] == CellType.Dead)
+						{
+							canvas.DrawRect(
+								(float)(x * caScale),
+								(float)(y * caScale),
+								(float)caScale,
+								(float)caScale,
+								new SKPaint { Color = SKColors.SlateGray.WithAlpha(128), Style = SKPaintStyle.Fill }
+							);
+						}
+					}
+				}
+			}
+
 			using var image = FastImage.WrapSKBitmap(bitmap, scale: 1);
 			SaveImage(image, $"{prefix} - mark regions {markRegions}");
 		}
 	}
+
+	[Test, Explicit]
+	public void DetermineDistanceFromWater()
+	{
+		const string prefix = "distance from water";
+		DeleteImages(prefix);
+
+		const int size = 2048;
+
+		var voronoiParams = (
+			Width: size,
+			Height: size,
+			NumberOfSites: 1000,
+			RelaxIterations: 3,
+			RelaxStrength: 0.25f,
+			PointMethod: PointGenerationMethod.Uniform
+		);
+		var caParams = (ProbabalityAlive: 0.48, Size: 32, RandomSeed: 0);
+
+		var random = new Random(caParams.RandomSeed);
+
+		var caBoard = new CellBoard(new Size(caParams.Size, caParams.Size))
+			.Fill(random, probabilityAlive: caParams.ProbabalityAlive)
+			.MakeBorderAlive(thickness: 1)
+			.GenerateStandardCave();
+
+		var caScale = (double)voronoiParams.Width / caBoard.Width;
+
+		VoronoiPlane plane = new(0, 0, voronoiParams.Width, voronoiParams.Height);
+		plane.GenerateRandomSites(voronoiParams.NumberOfSites, voronoiParams.PointMethod);
+		plane.Tessellate();
+		plane.Relax(voronoiParams.RelaxIterations, voronoiParams.RelaxStrength);
+
+		var sites = plane.Sites.Where(s => !s.SkippedAsDuplicate).ToList();
+
+		var regions = sites
+			.Select(s => new Region(
+				s,
+				IsLand: caBoard[new Position((int)(s.X / caScale), (int)(s.Y / caScale))] == CellType.Dead,
+				DistanceFromWater: -1
+			))
+			.ToList();
+
+		var bitmap = new SKBitmap(voronoiParams.Width, voronoiParams.Height);
+		using var canvas = new SKCanvas(bitmap);
+
+		// Draw regions
+		foreach (var region in regions)
+		{
+			// Site polygons
+
+			var color = region.IsLand ? SKColors.LightGreen : SKColors.RoyalBlue;
+
+			var points = region.Site.ClockwisePoints.Select(p => new SKPoint((float)p.X, (float)p.Y)).ToArray();
+			using var path = new SKPath();
+			path.AddPoly(points, close: true);
+			canvas.DrawPath(path, new SKPaint { Color = color, Style = SKPaintStyle.Fill });
+
+			// Distance text
+			if (region.IsLand)
+			{
+				var text = region.DistanceFromWater.ToString();
+
+				var font = new SKFont();
+				font.MeasureText(text, out var textBounds);
+				var centeredY = (float)region.Site.Y - (textBounds.Top + textBounds.Bottom) / 2;
+				canvas.DrawText(
+					text,
+					(float)region.Site.X,
+					centeredY,
+					SKTextAlign.Center,
+					font,
+					new SKPaint { Color = SKColors.Black }
+				);
+			}
+		}
+
+		// Draw region edges
+		foreach (var edge in plane.Edges)
+		{
+			canvas.DrawLine(
+				(float)edge.Start.X,
+				(float)edge.Start.Y,
+				(float)edge.End.X,
+				(float)edge.End.Y,
+				new SKPaint { Color = SKColors.Black, StrokeWidth = 1 }
+			);
+		}
+
+		using var image = FastImage.WrapSKBitmap(bitmap, scale: 1);
+		SaveImage(image, $"{prefix}");
+	}
+
+	private sealed record Region(VoronoiSite Site, bool IsLand, int DistanceFromWater);
 }
